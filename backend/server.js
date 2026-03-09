646923938b00dfb61fa048ed296919232fd9f84d
Kruti <you@example.com>
2026-03-09 11:57:36 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import Inquiry from "./model.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/dhwanikaDB")
-.then(()=>console.log("MongoDB Connected"));
+mongoose.connect("mongodb://127.0.0.1:27017/dhwanika")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
 
-app.post("/api/inquiry",async(req,res)=>{
-
-try{
-
-const data = new Inquiry(req.body);
-await data.save();
-
-res.json({message:"Saved"});
-
-}catch(err){
-res.status(500).json(err);
-}
-
+app.post("/api/inquiry", async (req,res)=>{
+  try{
+    console.log(req.body);
+    res.json({message:"Data received"});
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000");
+app.listen(5000, ()=>{
+  console.log("Server running");
 });
