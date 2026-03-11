0e920e35c22ac21546ae7363b7bfd3f77cb9b1f7
Kruti <you@example.com>
2026-03-11 17:39:29 +0530

    Contact us page

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import packageRoutes from "./routes/packages.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/packages",packageRoutes);
 
 mongoose.connect("mongodb://127.0.0.1:27017/dhwanika")
-.then(()=>console.log("MongoDB Connected Successfully😊"))
+.then(()=>console.log("MongoDB Connected Successfully 😊"))
 .catch(err=>console.log(err));
 
-app.post("/api/inquiry", async (req,res)=>{
+app.post("/api/inquiry",(req,res)=>{
   try{
     console.log(req.body);
     res.json({message:"Data received"});
@@ -23,5 +21,5 @@ app.post("/api/inquiry", async (req,res)=>{
 });
 
 app.listen(5000, ()=>{
-  console.log("Server running");
+  console.log("Server running on port 5000");
 });
