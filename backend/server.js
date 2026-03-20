3589c11535f4b2a84fc98d11307244b470f026aa
Kruti <you@example.com>
2026-03-20 15:47:55 +0530

    Changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+
 const app = express();
 
 app.use(cors());
@@ -11,15 +12,30 @@ mongoose.connect("mongodb://127.0.0.1:27017/dhwanika")
 .then(()=>console.log("MongoDB Connected Successfully 😊"))
 .catch(err=>console.log(err));
 
-app.post("/api/inquiry",(req,res)=>{
+// app.post("/api/inquiry",(req,res)=>{
+//   try{
+//     console.log(req.body);
+//     res.json({message:"Data received"});
+//   }catch(err){
+//     res.status(500).json(err);
+//   }
+// });
+app.post("/api/inquiry", async (req,res)=>{
   try{
-    console.log(req.body);
-    res.json({message:"Data received"});
+
+    const newInquiry = new Inquiry(req.body);
+
+    await newInquiry.save();
+
+    console.log("Inquiry Saved:", req.body);
+
+    res.json({message:"Inquiry saved successfully"});
+
   }catch(err){
+    console.log(err);
     res.status(500).json(err);
   }
 });
-
 app.listen(5000, ()=>{
   console.log("Server running on port 5000");
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <>
   <Navbar/>
     <Routes>    
-
+ 
 
       <Route path="/" element={<Home />} />
       <Route path="/privacy" element={<PrivacyPolicy />} />
