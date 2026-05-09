fbeb569de5b2a3fd3a0a20ed16498b9aaf9ba9a1
Kruti <you@example.com>
2026-05-09 14:19:05 +0530

    changed

--- frontend/src/pages/VisaDetails.jsx
+++ frontend/src/pages/VisaDetails.jsx
@@ -347,7 +347,6 @@ export default function VisaDetails() {
             >
               {loading ? "Submitting..." : "Submit Inquiry"}
             </button>
-
           </form>
         </div>
       </div>
