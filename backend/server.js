a398205abd13fccf862658b50fdd359a7206e2d1
Kruti <you@example.com>
2026-05-09 18:40:23 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -9,34 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://krutikpanchal2322002_db_user:CxUsPp7K16nxQ8HM@ac-7qptq0q-shard-00-00.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-01.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-02.fyrea24.mongodb.net:27017/?ssl=true&replicaSet=atlas-isxfi1-shard-0&authSource=admin&appName=Cluster0")
-.then(()=>console.log("MongoDB Connected Successfully 😊"))
-.catch(err=>console.log(err));
+mongoose
+  .connect(
+    "mongodb://krutikpanchal2322002_db_user:CxUsPp7K16nxQ8HM@ac-7qptq0q-shard-00-00.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-01.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-02.fyrea24.mongodb.net:27017/?ssl=true&replicaSet=atlas-isxfi1-shard-0&authSource=admin&appName=Cluster0",
+  )
+  .then(() => console.log("MongoDB Connected Successfully 😊"))
+  .catch((err) => console.log(err));
 
-// app.post("/api/inquiry",(req,res)=>{
-//   try{
-//     console.log(req.body);
-//     res.json({message:"Data received"});
-//   }catch(err){
-//     res.status(500).json(err);
-//   }
-// });
-app.post("/api/inquiry", async (req,res)=>{
-  try{
-
+app.post("/api/inquiry", async (req, res) => {
+  try {
     const newInquiry = new Inquiry(req.body);
 
     await newInquiry.save();
 
     console.log("Inquiry Saved:", req.body);
 
-    res.json({message:"Inquiry saved successfully"});
-
-  }catch(err){
+    res.json({ message: "Inquiry saved successfully" });
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
-app.listen(5000, ()=>{
+app.listen(5000, () => {
   console.log("Server running on port 5000");
-}); 
+});
