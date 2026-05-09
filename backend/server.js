3279fa5183145b08bf9709035a41f4573fb2ff16
Kruti <you@example.com>
2026-05-09 19:07:42 +0530

    Inq on Mail added Feature

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import nodemailer from "nodemailer";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -8,7 +9,13 @@ const app = express();
 // CxUsPp7K16nxQ8HM
 app.use(cors());
 app.use(express.json());
-
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "visas@dhwanikaoverseas.in",
+    pass: "hodbnygtkfonjpwm"
+  }
+});
 mongoose
   .connect(
     "mongodb://krutikpanchal2322002_db_user:CxUsPp7K16nxQ8HM@ac-7qptq0q-shard-00-00.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-01.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-02.fyrea24.mongodb.net:27017/?ssl=true&replicaSet=atlas-isxfi1-shard-0&authSource=admin&appName=Cluster0",
@@ -19,12 +26,29 @@ mongoose
 app.post("/api/inquiry", async (req, res) => {
   try {
     const newInquiry = new Inquiry(req.body);
-
     await newInquiry.save();
 
-    console.log("Inquiry Saved:", req.body);
+    const { firstName, lastName, email, phone, service } = req.body;
 
-    res.json({ message: "Inquiry saved successfully" });
+    const mailOptions = {
+      from: "visas@dhwanikaoverseas.in",
+      to: "visas@dhwanikaoverseas.in", // where YOU receive inquiry
+      subject: "🔥 New Inquiry - Dhwanika Overseas",
+      html: `
+        <h2>New Inquiry Received</h2>
+        <p><b>Name:</b> ${firstName} ${lastName}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <p><b>Service:</b> ${service}</p>
+      `
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    console.log("✅ Inquiry Saved & Email Sent");
+
+    res.json({ message: "Inquiry sent successfully" });
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
