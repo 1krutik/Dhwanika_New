9aaaf370ce4187bf2527b32c0ed896b9d2eac8b7
Kruti <you@example.com>
2026-05-09 18:29:49 +0530

    changed Data Base To Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import Inquiry from "./models/Inquiry.js";
 
 const app = express();
-
+// krutikpanchal2322002_db_user
+// CxUsPp7K16nxQ8HM
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/dhwanika")
+mongoose.connect("mongodb://krutikpanchal2322002_db_user:CxUsPp7K16nxQ8HM@ac-7qptq0q-shard-00-00.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-01.fyrea24.mongodb.net:27017,ac-7qptq0q-shard-00-02.fyrea24.mongodb.net:27017/?ssl=true&replicaSet=atlas-isxfi1-shard-0&authSource=admin&appName=Cluster0")
 .then(()=>console.log("MongoDB Connected Successfully 😊"))
 .catch(err=>console.log(err));
 
