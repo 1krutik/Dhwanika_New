297b9f7e0f92672cf89743d230995f06371cffaa
Kruti <you@example.com>
2026-08-21 18:13:34 +0530

    Changed Discription

--- frontend/src/pages/TravelCards.jsx
+++ frontend/src/pages/TravelCards.jsx
@@ -5,8 +5,8 @@ import destinations from "../components/Destinations/data";
 
 const TravelCards = () => {
   return (
-    <div className="px-4 md:px-10 py-10 bg-gray-50 min-h-screen">
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 text-blue-600">Explore Destinations</h1>
+    <div className="px-4 md:px-10 py-3 bg-gray-50 min-h-screen">
+      <h3 className="text-3xl md:text-4xl font-bold text-center mb-5 text-blue-600">Popular Holiday Destinations</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {destinations.map((destination, index) => (
           <DestinationCard key={index} destination={destination} />
